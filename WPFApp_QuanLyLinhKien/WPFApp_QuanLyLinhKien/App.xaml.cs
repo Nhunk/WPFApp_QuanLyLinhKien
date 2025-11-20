016451d1c6bd0e39c8fa64c12644c77549583be3
Nhunk <anhnhung.edu@gmail.com>
2025-11-20 18:45:26 +0700

    add WPF project

---
+++ WPFApp_QuanLyLinhKien/WPFApp_QuanLyLinhKien/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFApp_QuanLyLinhKien
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
